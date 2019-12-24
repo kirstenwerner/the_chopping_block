7b1003c27bfa0ae5a207d6382cf4bb4ef06d657d
kirstenwerner <kirstenmwerner@gmail.com>
2019-12-23 16:06:05 -0800

    added timestamps to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_235626) do
+ActiveRecord::Schema.define(version: 2019_12_24_000245) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
