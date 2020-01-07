a4c1ff45a203f694c797037fdf96fc0aff893a4f
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-06 17:25:55 -0800

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_24_000245) do
+ActiveRecord::Schema.define(version: 2020_01_07_012049) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_12_24_000245) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
