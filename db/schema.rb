63c5d04f1a335fb5683e51bc61436a2b3f18dafb
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-09 16:38:03 -0800

    added secure password with bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_205841) do
+ActiveRecord::Schema.define(version: 2020_01_10_002839) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_205841) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
