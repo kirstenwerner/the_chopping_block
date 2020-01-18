731b7dc70c73f38b6427f36f6d0b99c848c32d2a
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-18 19:16:09 +0100

    added admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_002839) do
+ActiveRecord::Schema.define(version: 2020_01_18_125007) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_10_002839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
