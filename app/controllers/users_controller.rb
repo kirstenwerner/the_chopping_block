8e8c8992da2cb018ab5be22048f01d37782c1f53
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-18 13:33:57 +0100

    sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save 
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to The Chopping Block, #{@user.username}!"
-            redirect_to recipes_path
+            redirect_to user_path(@user)
         else 
             render 'new'
         end 
