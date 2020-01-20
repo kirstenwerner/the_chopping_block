e38857d46366efb3de1f234bd45b4b98a9b1e512
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-20 19:39:00 +0100

    create recipe-category join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_161358) do
+ActiveRecord::Schema.define(version: 2020_01_20_182930) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_categories", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "category_id"
   end
 
   create_table "recipes", force: :cascade do |t|
