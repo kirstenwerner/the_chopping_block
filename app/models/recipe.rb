0ac027b835cffd00e9e3fff70a4ba4dd5cef0a21
kirstenwerner <kirstenmwerner@gmail.com>
2020-01-20 19:39:27 +0100

    add many-to-many relationships between categories and recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ActiveRecord::Base 
     belongs_to :user 
+    has_many :recipe_categories
+    has_many :categories, through: :recipe_categories
 
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :details, presence: true, length: { minimum: 10, maximum: 1000 }
